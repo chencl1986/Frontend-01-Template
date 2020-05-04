b421de1b6e78bba39919a9b738aa15cf0bb38e19
chencl1986 <chencl1986@qq.com>
2020-05-04 16:31:52 +0800

    feat(): 完成week03 2. 重学 JavaScript 语句 Statement笔记

--- week03/2.js
+++ week03/2.js
@@ -85,7 +85,17 @@ var o = function foo(params) {
   
 } */
 
-/* // 函数声明
+/* // 类声明
+class cls {
+  
+}
+
+// 类表达式
+var cls = class {
+  
+} */
+
+/* // Generator函数声明
 function* foo() {
   yield 1;
   yield 2;
@@ -96,8 +106,16 @@ function* foo() {
   }
 }
 
-// 函数表达式
-var o = function* foo(params) {}; */
+var gen = foo();
+console.log(gen.next()); // { value: 1, done: false }
+console.log(gen.next()); // { value: 2, done: false }
+console.log(gen.next()); // { value: 3, done: false }
+console.log(gen.next()); // { value: 4, done: false }
+console.log(gen.next()); // { value: 5, done: false }
+console.log(gen.next()); // { value: 6, done: false }
+
+// Generator函数表达式
+var o = function* foo() {}; */
 
 /* function sleep(timeout) {
   return new Promise((resolve, reject) => {
@@ -123,36 +141,51 @@ void (async function () {
   });
 }
 
-(async function* foo() {
+async function* foo() {
   var i = 0;
   while (true) {
-    console.log(i++);
+    console.log(i++); // i
     yield i;
     await sleep(1000);
   }
-})();
+}
 
 var g = foo();
-g.next().next().next().next().next();
+g.next();
+g.next();
+g.next();
+g.next();
+g.next();
 
-void async function () {
+void (async function () {
   var g = foo();
-  console.log(await g.next());
-}
-*/
+  console.log(await g.next()); // { value: 1, done: false }
+  console.log(await g.next()); // { value: 2, done: false }
+  console.log(await g.next()); // { value: 3, done: false }
+  console.log(await g.next()); // { value: 4, done: false }
+  console.log(await g.next()); // { value: 5, done: false }
+})();
+
+void (async function () {
+  var g = foo();
+  for await (let e of g) {
+    console.log(e); // 无限输出i
+  }
+})(); */
 
 /* var x = 0;
 function foo() {
   var o = {x: 1};
   x = 2;
   with (o) {
-    var x = 3;
+    var x = 3; // 改变了o.x的值
   }
-  console.log(x);
+  console.log(x); // 2，由于with内部有var声明，相当于在函数内部声明了一个x，因此打印了内部的x
+  console.log(o); // { x: 3 }，with内部修改了o的属性
 }
 
 foo();
-console.log(x); */
+console.log(x); // 0，由于函数内部声明了变量x，外部的变量x不会被修改 */
 
 /* var x = 0;
 function foo() {
@@ -161,67 +194,77 @@ function foo() {
   with (o) {
     x = 3;
   }
-  console.log('inner', x);
+  console.log('inner', x); // 2，函数内部没有var声明x，因此直接修改了外部变量x
+  console.log(o); // { x: 3 }，with内部修改了o的属性
 }
 
 foo();
-console.log('outer', x); */
+console.log('outer', x); // 2，函数内部没有var声明x，外部变量x被修改。 */
 
 /* var x = 0;
 function foo() {
   var o = {x: 1};
   x = 2;
   if (false) {
-    var x = 3;
+    var x = 3; // 即使代码不会运行，变量也会声明
   }
-  console.log('inner', x);
+  console.log('inner', x); // 2
 }
 
 foo();
-console.log('outer', x); */
+console.log('outer', x); // 0 */
 
 /* var x = 0;
 function foo() {
   var o = {x: 1};
   x = 2;
-
-  if (false) {
-    var x = 3;
-  }
-
-  console.log('inner', x);
+  console.log('inner', x); // 2
+  return;
+  var x = 3; // 即使代码不会运行，变量也会声明
 }
 
 foo();
-console.log('outer', x); */
+console.log('outer', x); // 0 */
 
 /* void (function foo() {
-  foo2();
-  console.log(i);
+  foo2(); // 2，函数可被调用，因此输出2
+  console.log(i); // undefined
 
   return;
-  var i = 1;
+  var i = 1; // 即使代码不会运行，变量也会声明
   function foo2() {
-    console.log(2);
+    console.log(2); // 2
   }
-})();
- */
+})(); */
 
 /* var cls1 = 0;
 function foo() {
   cls1 = 2; // Cannot access 'cls1' before initialization
   class cls1 {}
-  // class cls1 {} // Identifier 'cls1' has already been declared
+  class cls1 {} // Identifier 'cls1' has already been declared
 }
 
 foo();
-console.log(cls1);
- */
+console.log(cls1); */
+
+/* var cls1 = 0;
+
+function foo() {
+  console.log(cls1);
+  const cls1; // SyntaxError: Missing initializer in const declaration
+} */
 
 /* function foo() {
   console.log(this);
   return {a: 1};
 }
 
-foo();
-new foo(); */
+foo(); // window or global
+new foo(); // {a: 1} */
+
+/* var o = [];
+o[100] = 1;
+console.log(o.length); // 101
+console.log(Object.getOwnPropertyDescriptor(o, 'length')); // { value: 101, writable: true, enumerable: false, configurable: false } */
+
+/* Object.setPrototypeOf(Object.prototype, {a: 1}); // TypeError: Immutable prototype object '#<Object>' cannot have their prototype set */
