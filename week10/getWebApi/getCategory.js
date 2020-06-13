2727a23bffcc15c38c2efea65f9df93762cf3f82
chencl1986 <chencl1986@qq.com>
2020-06-13 08:52:33 +0800

    feat(): 更新获取WebAPI方法

--- week10/getWebApi/getCategory.js
+++ week10/getWebApi/getCategory.js
@@ -1,19 +1,24 @@
-var treeStr = `${document.querySelector('.titlebar-container .title').innerText}\n\tDescription`;
+var treeStr = `${
+  document.querySelector('.titlebar-container .title').innerText
+}\n\tDescription`;
 var propertiesStr = '';
 var constructorStr = '';
 var wikiArticle = document.querySelector('#wikiArticle');
 var index = 0;
 var resolveTitle = true;
 var resolveAttr = '';
+var lastResolveAttr = '';
 var resolveStr = {
   Properties: '',
   Constructor: '',
   Methods: '',
+  Events: '',
 };
 var attrs = {
   Properties: true,
   Constructor: true,
   Methods: true,
+  Events: true,
 };
 
 while (index < wikiArticle.children.length) {
@@ -29,7 +34,20 @@ while (index < wikiArticle.children.length) {
     if (attrs[attr]) {
       resolveTitle = false;
       resolveAttr = attr;
+      lastResolveAttr = '';
       resolveStr[attr] = `\n\t${ele.innerText}`;
+    }
+  }
+
+  if (tagName === 'h3') {
+    if (lastResolveAttr) {
+      resolveStr[lastResolveAttr] += `\n\t\t${ele.innerText}`;
+    }
+  }
+
+  if (tagName === 'p') {
+    if (lastResolveAttr) {
+      resolveStr[lastResolveAttr] += `\n\t\t\t${ele.innerText}`;
     }
   }
 
@@ -51,10 +69,57 @@ while (index < wikiArticle.children.length) {
             resolveStr[resolveAttr] += `\n\t\t${child.innerText}`;
           }
         } else {
-          resolveStr[resolveAttr] += `\n\t\t\t${child.innerText}`;
+          if (child.innerText.match('\n\n')) {
+            resolveStr[resolveAttr] += `\n\t\t\t${child.innerText.replace(
+              '\n\n',
+              '\n\t\t\t',
+            )}`;
+          } else if (child.innerText.match('\n')) {
+            resolveStr[resolveAttr] += `\n\t\t\t${child.innerText.replace(
+              '\n',
+              '\n\t\t\t',
+            )}`;
+          } else {
+            resolveStr[resolveAttr] += `\n\t\t\t${child.innerText}`;
+          }
         }
       });
+      lastResolveAttr = resolveAttr;
       resolveAttr = '';
+    }
+
+    if (lastResolveAttr) {
+      Array.from(ele.children).forEach((child) => {
+        if (child.tagName.toLowerCase() === 'dt') {
+          if (child.children) {
+            Array.from(child.children).forEach((c, i) => {
+              if (!i) {
+                resolveStr[lastResolveAttr] += `\n\t\t\t${c.innerText}`;
+              } else {
+                if (c.innerText) {
+                  resolveStr[lastResolveAttr] += `\n\t\t\t\t${c.innerText}`;
+                }
+              }
+            });
+          } else {
+            resolveStr[lastResolveAttr] += `\n\t\t\t${child.innerText}`;
+          }
+        } else {
+          if (child.innerText.match('\n\n')) {
+            resolveStr[lastResolveAttr] += `\n\t\t\t\t${child.innerText.replace(
+              '\n\n',
+              '\n\t\t\t\t',
+            )}`;
+          } else if (child.innerText.match('\n')) {
+            resolveStr[lastResolveAttr] += `\n\t\t\t\t${child.innerText.replace(
+              '\n',
+              '\n\t\t\t\t',
+            )}`;
+          } else {
+            resolveStr[lastResolveAttr] += `\n\t\t\t\t${child.innerText}`;
+          }
+        }
+      });
     }
   }
 
@@ -64,6 +129,7 @@ while (index < wikiArticle.children.length) {
 treeStr += resolveStr.Constructor;
 treeStr += resolveStr.Properties;
 treeStr += resolveStr.Methods;
+treeStr += resolveStr.Events;
 
 treeStr += '\n\tSpecifications';
 
@@ -90,5 +156,5 @@ Array.from(
   });
 });
 
-// console.log(treeStr);
+console.log(treeStr);
 copy(treeStr);
