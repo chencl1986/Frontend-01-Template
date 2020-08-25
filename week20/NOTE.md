ecdcf5367ab38bfc99dbed1682d6e2495fbf624d
chencl1986 <chencl1986@qq.com>
2020-08-25 23:56:28 +0800

    feat(week20): 发布系统增加基于OAuth的多用户登录

--- week20/NOTE.md
+++ week20/NOTE.md
@@ -2,47 +2,16 @@
 
 ## PhantomJS
 
-1. PhantomJS的内容我觉得效果不大好，由于其支持的ECMAScript版本实在太老，并且在其内部无法进行`console.log`输出内容，导致调试即为困难。
-2. 不知是否改为Puppeteer效果会更好一些，但Puppeteer的依赖似乎比较大，可以提前通知学员下载。
+1. PhantomJS 的内容我觉得效果不大好，由于其支持的 ECMAScript 版本实在太老，并且在其内部无法进行`console.log`输出内容，导致调试即为困难。
+2. 不知是否改为 Puppeteer 效果会更好一些，但 Puppeteer 的依赖似乎比较大，可以提前通知学员下载。
 
 ## 项目发布
 
-1. 我认为老师的OAuth登录流程有个问题，就是只支持了一个用户登录。
-2. 实际项目中可以在获取token之后，直接获取用户信息，判断是否正常用户，再将用户信息存入Session，即可支持多用户同时访问，Koa的示例代码如下：
+我认为老师的 OAuth 登录流程有个问题，就是只支持了一个用户登录。
 
-```
-import session from 'koa-session';
+我针对这个问题，做了一些改动：
 
-// 使用koa-session中间件
-server.use(session(
-  {
-    maxAge: 30 * 60 * 1000, // 登录状态30分钟后失效
-    rolling: true,
-    renew: false,
-  },
-  app
-))
-
-// 用户认证
-router.get('/auth', ctx => {
-  fetch('/user', {})
-    .then(response => {
-      return response.json()
-    })
-    .then(result => {
-      // 登录后可以通过session判断当前用户的登录状态
-      ctx.session.userInfo = result
-    })
-})
-
-// 用户进行发布
-router.get('/publish', async ctx => {
-  if (!ctx.session.userInfo) {
-    await ctx.render(500) // 使用模板引擎，渲染一个500报错页面
-  } else {
-    // 进行发布
-  }
-})
-```
-
-由于实现起来会复杂很多，而且改动比较大，考虑到实际用处不大，这里就不进行代码上的优化了。
+1. 启动`publish-new/publish-vanilla/index.js`时，打开页面`http://localhost:8081/index.html`。
+2. 此时从 cookie 中读取 token，如果存在则可以进行发布，不存在则跳转到 GitHub 登录。
+3. 访问`http://localhost:8081/auth`时，同样判断 token 存在与否，决定是否要登录。
+4. 另外，设置了 `Cookie` 有效期为 30 分钟。
--- week20/publish-new/publish-vanilla/index.js
+++ week20/publish-new/publish-vanilla/index.js
@@ -3,6 +3,22 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const unzip = require('unzipper');
+const child_process = require('child_process');
+
+// 启动服务器后打开首页
+child_process.exec(`start http://localhost:8081/index.html`);
+
+// 获得客户端的Cookie
+function getCookies(req) {
+  let cookies = {};
+  req.headers.cookie &&
+    req.headers.cookie.split(';').forEach(function (Cookie) {
+      let parts = Cookie.split('=');
+      cookies[parts[0].trim()] = (parts[1] || '').trim();
+    });
+  console.log('cookies', cookies);
+  return cookies;
+}
 
 function auth(req, res) {
   let code = req.url.match(/code=([^&]+)/)[1];
@@ -29,13 +45,16 @@ function auth(req, res) {
 
     response.on('data', (d) => {
       let result = d.toString().match(/access_token=([^&]+)/);
+      console.log('access_token result', result);
 
       if (result) {
         let token = result[1];
         console.log('token', token);
 
         res.writeHead(200, {
-          'Set-Cookie': token,
+          'Set-Cookie': `token=${token}; Expires=${new Date(
+            new Date().getTime() + 30 * 60 * 1000,
+          ).toUTCString()}`,
           access_token: token,
           'Content-Type': 'text/html',
         });
@@ -61,8 +80,36 @@ function auth(req, res) {
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
+  if (req.url === '/index.html') {
+    const cookies = getCookies(req);
+    // 进入index时，判断cookie中是否有token，有则表示已登录，可以发布
+    if (cookies.token) {
+      res.end(
+        `<a href="http://localhost:8080/publish?token=${cookies.token}">publish</a>`,
+      );
+      return;
+    } else {
+      // cookie中没有token，则跳转到github登录
+      const redirect_uri = encodeURIComponent('http://localhost:8081/auth');
+      res.writeHead(301, {
+        Location: `https://github.com/login/oauth/authorize?client_id=d0a5737dce00dc2ca997&redirect_uri=${redirect_uri}&state=abc123`,
+      });
+      res.end();
+      return;
+    }
+  }
   if (req.url.match(/^\/auth/)) {
-    return auth(req, res);
+    const cookies = getCookies(req);
+    // 访问/auth时，判断cookie中是否有token，有则表示已登录，可以发布
+    if (cookies.token) {
+      res.end(
+        `<a href="http://localhost:8080/publish?token=${cookies.token}">publish</a>`,
+      );
+      return;
+    } else {
+      // 若没有token，则进行认证
+      return auth(req, res);
+    }
   }
   if (req.url.match(/^\/favicon.ico/)) {
     res.writeHead(404, {'Content-Type': 'text/plain'});
@@ -74,7 +121,7 @@ const server = http.createServer((req, res) => {
   let filename = matched && matched[1];
   const filePath = path.resolve(__dirname, `../server/public/${filename}`);
   const writeStream = fs.createWriteStream(filePath); */
-  console.log(req.headers);
+  console.log('req.headers', req.headers);
 
   const options = {
     hostname: 'api.github.com',
